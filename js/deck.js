dc5b04b02f33e260ac37aa2522829b56b6a9bf07
THOMAS FASSIH <thomasfassih@gmail.com>
2023-03-16 03:59:58 -0500

    Update deck.js

--- js/deck.js
+++ js/deck.js
@@ -60,14 +60,14 @@ function deck(){
     return cards;
 }
 
-function shuffleArr(o) {
+function shuffle(o) {
     for(let j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
 
 function init(){
     let deckContainer = new deck();
-    deckContainer = shuffleArr(deckContainer);
+    deckContainer = shuffle(deckContainer);
     let container = document.createElement('div');
     container.className = 'container';
     container.id = "container";
